c93c06655e0f450672c600f19e86d71f12b8652b
Vaclav Vavra <mr.vaclav.vavra@gmail.com>
2023-12-05 22:43:40 +0100

    updating the toml with strategy 1

--- fake_compute.py
+++ fake_compute.py
@@ -1,15 +1,16 @@
+import argparse
 import time
 
+import cv2 as cv
+import matplotlib.pyplot as plt
+import numpy as np
 from pyquaternion import Quaternion
 
-from common.data_parsing import save, get_cached_data, ConfStatic, read_toml
-from data_save import save_data
-import cv2 as cv
-import numpy as np
-import argparse
-
+from metadata import *
 from common.common_transforms import project_metropolis
+from common.data_parsing import save, get_cached_data, ConfStatic, read_toml
 from common.vanishing_point import change_x_3d_arkit, change_r_arkit
+from common.fitting import fit_min_area_rect
 
 
 def scene_args():
@@ -20,97 +21,263 @@ def scene_args():
         default="./out_data",
         help="output folder"
     )
+    parser.add_argument("--max_entries", type=int, default=None)
     parser.add_argument("--conf_base_path", type=str, default=None)
-    parser.add_argument("--cache_every_other", type=int, default=10000)
+    parser.add_argument("--cache_every_other", type=int, default=5000)
     parser.add_argument("--format_suffix", type=str, default=ConfStatic.toml_suffix)
     args = parser.parse_args()
     print(f"Args: {args}")
     return args
 
 
-BOXES_2D_KEY = "segmented_boxes_2d"
+BOXES_2D_KEY = "segmented_boxes_2d_1"
 WIDTHS_HEIGHTS_KEY = "segmented_wh"
 
 
-def compute(fc=None):
+def simple_show_img(img_l, title=None):
+    plt.figure()
+    plt.title(title)
+    plt.imshow(img_l)
+    plt.show()
+    plt.close()
+
+
+def get_scale(original_img, segmemtation_map):
+    or_size = original_img.shape[:2]
+    size = segmemtation_map.shape[:2]
+    scale = float(or_size[0]) / float(size[0])
+    scale2 = float(or_size[1]) / float(size[1])
+    # MAY not hold actually
+    assert np.isclose(scale, scale2)
+    return scale
+
+
+def get_simple_path_prefix(config_entry, out_data_root):
+
+    img_path = config_entry['orig_file_path']
+    # hack - ../../download/... -> ./download/.
+    img_path = img_path[4:]
+    print(f"path: {img_path}")
+    assumed_prefix_l = len("./download/3dod/Training/")
+    return f"{out_data_root}/{img_path[assumed_prefix_l:]}"[:-4]
+
+
+def get_imgs(config_entry, out_data_root):
+
+    img_path = config_entry['orig_file_path']
+
+    # # hack - ../../download/... -> ./download/.
+    img_path = img_path[4:]
+    # print(f"path: {img_path}")
+    # assumed_prefix_l = len("./download/3dod/Training/")
+    # simple_path_prefix = f"{out_data_root}/{img_path[assumed_prefix_l:]}"[:-4]
+    simple_path_prefix = get_simple_path_prefix(config_entry, out_data_root)
+
+    original_img = cv.imread(img_path)
+    # simple_show_img(original_img, "original image")
+
+    segments_info = read_toml(f"{simple_path_prefix}_data.toml")
+    segm_vis_img = cv.imread(f"{simple_path_prefix}_segmentation_original.png")
+    # simple_show_img(segm_vis_img, "visualizer")
+
+    segmentation_map = cv.imread(f"{simple_path_prefix}_segmentation.png").astype(int)[:, :, 0]
+    contrast = 230 // segmentation_map.max()
+    segm_contrast_img = segmentation_map * contrast
+    segm_contrast_img[segm_contrast_img == 0] = 255
+    # simple_show_img(segm_contrast_img, "segm_contrast_img")
+    # simple_show_img(segmentation_map, "segmentation_map")
+    return segments_info, segmentation_map, segm_contrast_img, segm_vis_img, original_img
+
+
+if_thing_stuff_maps = {
+    True: (thing_dataset_id_to_contiguous_id, thing_classes),
+    False: (stuff_dataset_id_to_contiguous_id, stuff_classes),
+}
+
+
+def get_boxes(config_entry, segments_info, segmemtation_map, scale):
+
+    K = np.array(config_entry['K'].copy())
+    R_cs_l = config_entry['R_cs'].copy()
+    R_gt = Quaternion(R_cs_l).rotation_matrix
+    R_gt = change_r_arkit(R_gt)
+    t_gt = np.array(config_entry['t_cs'].copy())
+    X_i = np.array(config_entry['X_i'].copy())
+    X_i = change_x_3d_arkit(X_i)
+    X_i_test = np.array(config_entry["X_i_new"].copy())
+    assert np.allclose(np.array(X_i_test), np.array(X_i))
+    x_i_gt = project_metropolis(K, R_gt, t_gt, X_i)
+    x_i_int_unscaled = np.round(x_i_gt / scale).astype(int)
+    x_i_int = np.round(x_i_gt).astype(int)
+
+    # even missing for these
+    categories = []
+    ds_categories = []
+    boxes_original = []
+    boxes_unscaled = []
+    # split these
+    x_i_int_unscaled_ret = []
+    x_i_int_ret = []
+    x_i_int_out_unscaled_ret = []
+    x_i_int_out_ret = []
+    for i, coord_x_y in enumerate(x_i_int_unscaled):
+
+        def clean():
+            boxes_unscaled.append([])
+            boxes_original.append([])
+            x_i_int_out_unscaled_ret.append(coord_x_y)
+            x_i_int_out_ret.append(x_i_int[i])
+            categories.append("not found")
+
+        ds_name = config_entry['names'][i]
+        ds_categories.append(ds_name)
+
+        if coord_x_y[0] < 0 or coord_x_y[0] >= segmemtation_map.shape[1] or \
+                coord_x_y[1] < 0 or coord_x_y[1] >= segmemtation_map.shape[0]:
+            clean()
+            continue
+        obj_idx = segmemtation_map[coord_x_y[1], coord_x_y[0]]
+        if obj_idx == 0:
+            clean()
+            continue
+
+        x_i_int_unscaled_ret.append(coord_x_y)
+        x_i_int_ret.append(x_i_int[i])
+
+        # object_info = "{'id': 1,
+        # 'isthing': False,
+        # 'category_id': 0,
+        # 'area': 112475.0,
+        # 'box': list[Float, 4, 2]}"
+        object_info = segments_info['objects'][obj_idx - 1]
+        assert object_info["id"] == obj_idx
+
+        # isthing = object_info["isthing"]
+        # well it is just using stuff IMHO
+        id_map, classes_map = if_thing_stuff_maps[False]
+        id = id_map[object_info["category_id"]]
+        assert id == object_info["category_id"]
+        category = classes_map[id]
+        categories.append(category)
+
+        pixels_to_fit = np.array(np.where(segmemtation_map == obj_idx)).T
+        box = fit_min_area_rect(pixels_to_fit)
+        boxes_unscaled.append(box.tolist().copy())
+        box *= scale
+        box = box.tolist()
+        boxes_original.append(box)
+
+        # assert
+        box_data = object_info["box"]
+        assert box_data == box
+
+
+    x_i_int_unscaled_ret = np.array(x_i_int_unscaled_ret)
+    x_i_int_ret = np.array(x_i_int_ret)
+    x_i_int_out_unscaled_ret = np.array(x_i_int_out_unscaled_ret)
+    x_i_int_out_ret = np.array(x_i_int_out_ret)
+    return categories, \
+           ds_categories, \
+           boxes_original, \
+           boxes_unscaled, \
+           x_i_int_unscaled_ret, \
+           x_i_int_ret, \
+           x_i_int_out_unscaled_ret, \
+           x_i_int_out_ret
+
+
+def visualize_image(img_to_show,
+                    x_i_proper,
+                    x_i_out,
+                    rectangles,
+                    title=None,
+                    save_path=None):
+    rectangles = [r for r in rectangles if len(r) > 0]
+    rectangles = np.array(rectangles)
+    _, ax = plt.subplots(1, 1)
+    # ax.set_axis_off()
+    ax.imshow(img_to_show)
+    if len(x_i_proper) > 0:
+        ax.plot(x_i_proper[:, 0], x_i_proper[:, 1], "bx", markersize="10", markeredgewidth=2)
+    if len(x_i_out) > 0:
+        ax.plot(x_i_out[:, 0], x_i_out[:, 1], "rx", markersize="10", markeredgewidth=2)
+    for rect in rectangles:
+        ax.plot(np.hstack((rect[:, 1], rect[0, 1])), np.hstack((rect[:, 0], rect[0, 0])), "y-.", linewidth=2)
+
+    plt.title(title)
+    if save_path:
+        plt.savefig(save_path, bbox_inches="tight")
+    show = True
+    if show:
+        plt.show()
+    plt.close()
+
+
+def compute():
 
     task = "panoptic"
     args = scene_args()
-
-    # toml_conf = read_toml(args.conf_base_path)
-    # configs/ARKitScenes=obj=2_max=100_sp=10.toml => conf_base_path = configs/ARKitScenes=obj=2_max=100
 
     start_time = time.time()
     data_entries, ready_entries, min_counts_map, config_read = get_cached_data(args.conf_base_path,
                                                                                format_suffix=ConfStatic.toml_suffix,
                                                                                out_log=True)
 
-    for e_i, e in enumerate(data_entries):
+    for e_i, config_entry in enumerate(data_entries):
 
-        if e.__contains__(BOXES_2D_KEY):
+        if config_entry.__contains__(BOXES_2D_KEY):
             continue
         else:
             ready_entries += 1
-            # if ready_entries > 10:
-            #     break
-
-        img_path = e['orig_file_path']
-        # hack - ../../download/... -> ./download/.
-        img_path = img_path[4:]
-        print(f"path: {img_path}")
-
-        assumed_prefix_l = len("./download/3dod/Training/")
-        simple_path_prefix = f"{args.out_data_root}/{img_path[assumed_prefix_l:]}"[:-4]
+            if args.max_entries and ready_entries > args.max_entries:
+                break
 
         # START FUNCTION
         # segments_info, segmentation, scale = fc(img_path, simple_path_prefix)
 
-        img = cv.imread(img_path)
-        or_size = img.shape[:2]
+        # a) read imgs, segmentation, segm_info
+        # i) infer the data (possibly read the data and assert ==)
+        # ii) read the data
+        segments_info, segmemtation_map, segm_contrast_img, segm_vis_img, original_img = \
+            get_imgs(config_entry, args.out_data_root)
 
-        toml_path = f"{simple_path_prefix}_data.toml"
-        segments_info = read_toml(toml_path)
+        # b) scale (merge?)
+        scale = get_scale(original_img, segmemtation_map)
 
-        # segm_original_path = f"{simple_path_prefix}_segmentation_original.png"
+        # c) all boxes/x_is...
+        categories, \
+        ds_categories, \
+        boxes_original, \
+        boxes_unscaled, \
+        x_i_int_unscaled, \
+        x_i_int, \
+        x_i_int_out_unscaled, \
+        x_i_int_out = get_boxes(config_entry, segments_info, segmemtation_map, scale)
 
-        segmentation = f"{simple_path_prefix}_segmentation.png"
-        # TODO type !!!
-        img_segm = cv.imread(segmentation)
-        size = img_segm.size[:2]
-
-        scale = float(or_size[0]) / float(size[0])
-        scale2 = float(or_size[1]) / float(size[1])
-        # MAY not hold actually
-        assert np.isclose(scale, scale2)
-
-        # -> segments_info, segmentation, scale
-        # ret = segments_info, img_segm, scale
+        # d) visualization
+        path_pref = get_simple_path_prefix(config_entry, args.out_data_root)
+        title = ", ".join([f"{dsc}->{c}" for dsc, c in zip(ds_categories, categories)])
+        title += f"\n ds categories: {ds_categories}"
+        visualize_image(segm_contrast_img,
+                        x_i_int_unscaled,
+                        x_i_int_out_unscaled,
+                        boxes_unscaled,
+                        title=title,
+                        save_path=f"{path_pref}_segmentation_contrast_boxes.png")
+        visualize_image(segm_vis_img,
+                        x_i_int_unscaled,
+                        x_i_int_out_unscaled,
+                        boxes_unscaled,
+                        title=title)
+        # visualize_image(original_img,
+        #                 x_i_int,
+        #                 x_i_int_out,
+        #                 boxes_original,
+        #                 title=title)
         # END FUNCTION
 
-        # MATCHING NO 1. -> visualize!!!
-        K = e['K']
-        R_cs_l = e['R_cs'].copy()
-        R_gt = Quaternion(R_cs_l).rotation_matrix
-        R_gt = change_r_arkit(R_gt)
-
-        t_cs_l = e['t_cs'].copy()
-        t_gt = np.array(t_cs_l)
-
-        X_i = e['X_i']
-        X_i = np.array(change_x_3d_arkit(np.array(X_i))).tolist()
-
-        x_i_gt = project_metropolis(K, R_gt, t_gt, X_i)
-
-        # CONTINUE: -> and check this!!
-        # this is already easy to debug!!!
-        x_i_gt = np.round(x_i_gt / scale)
-        obj_idx = img_segm[x_i_gt]
-
-        # this is probably better to have all bb's rather than computing them lazily
-        bb = segments_info[obj_idx]
-        # check the class
-        e['names'][obj_idx] == bb['class'] #??
-        # save_data(simple_path_prefix, panoptic_seg, segments_info, scale_to_or_0, segm_img)
+        # e) write to orig config (anything else?)
+        config_entry[BOXES_2D_KEY] = boxes_original
 
         if ready_entries % args.cache_every_other == 0:
             sp_file_path = f"{args.conf_base_path}_sp={ready_entries}"
@@ -133,4 +300,9 @@ def compute(fc=None):
 
 
 if __name__ == "__main__":
+    # assert
+    for k, v in stuff_dataset_id_to_contiguous_id.items():
+        assert k == v
+    for k, v in thing_dataset_id_to_contiguous_id.items():
+        assert k == v
     compute()
---
+++ metadata.py
@@ -0,0 +1,188 @@
+# METADATA: Metadata(evaluator_type='ade20k_panoptic_seg',
+#                    ignore_label=255,
+#                    image_root='datasets/ADEChallengeData2016/images/validation',
+#                    json_file='datasets/ADEChallengeData2016/ade20k_instance_val.json',
+#                    label_divisor=1000,
+#                    name='ade20k_panoptic_val',
+#                    panoptic_json='datasets/ADEChallengeData2016/ade20k_panoptic_val.json',
+#                    panoptic_root='datasets/ADEChallengeData2016/ade20k_panoptic_val',
+
+thing_colors = [[204, 5, 255], [230, 230, 230], [224, 5, 255], [150, 5, 61], [8, 255, 51],
+                [255, 6, 82], [255, 51, 7], [204, 70, 3], [0, 102, 200], [255, 6, 51], [11, 102, 255],
+                [255, 7, 71], [220, 220, 220], [8, 255, 214], [7, 255, 224], [255, 184, 6],
+                [10, 255, 71], [7, 255, 255], [224, 255, 8], [102, 8, 255], [255, 61, 6],
+                [255, 194, 7], [0, 255, 20], [255, 8, 41], [255, 5, 153], [6, 51, 255], [235, 12, 255],
+                [0, 163, 255], [250, 10, 15], [20, 255, 0], [255, 224, 0], [0, 0, 255], [255, 71, 0],
+                [0, 235, 255], [0, 173, 255], [0, 255, 245], [0, 255, 112], [0, 255, 133], [255, 0, 0],
+                [255, 163, 0], [194, 255, 0], [0, 143, 255], [51, 255, 0], [0, 82, 255], [0, 255, 41],
+                [0, 255, 173], [10, 0, 255], [173, 255, 0], [255, 92, 0], [255, 0, 245], [255, 0, 102],
+                [255, 173, 0], [255, 0, 20], [0, 31, 255], [0, 255, 61], [0, 71, 255], [255, 0, 204],
+                [0, 255, 194], [0, 255, 82], [0, 112, 255], [51, 0, 255], [0, 122, 255], [255, 153, 0],
+                [0, 255, 10], [163, 255, 0], [255, 235, 0], [8, 184, 170], [184, 0, 255], [255, 0, 31],
+                [0, 214, 255], [255, 0, 112], [92, 255, 0], [70, 184, 160], [163, 0, 255],
+                [71, 255, 0], [255, 0, 163], [255, 204, 0], [255, 0, 143], [133, 255, 0],
+                [255, 0, 235], [245, 0, 255], [255, 0, 122], [255, 245, 0], [214, 255, 0],
+                [0, 204, 255], [255, 255, 0], [0, 153, 255], [0, 41, 255], [0, 255, 204], [41, 0, 255],
+                [41, 255, 0], [173, 0, 255], [0, 245, 255], [0, 255, 184], [0, 92, 255], [184, 255, 0],
+                [255, 214, 0], [25, 194, 194], [102, 255, 0], [92, 0, 255]]
+
+stuff_colors = [[120, 120, 120], [180, 120, 120], [6, 230, 230], [80, 50, 50], [4, 200, 3],
+                [120, 120, 80], [140, 140, 140], [204, 5, 255], [230, 230, 230], [4, 250, 7],
+                [224, 5, 255], [235, 255, 7], [150, 5, 61], [120, 120, 70], [8, 255, 51], [255, 6, 82],
+                [143, 255, 140], [204, 255, 4], [255, 51, 7], [204, 70, 3], [0, 102, 200],
+                [61, 230, 250], [255, 6, 51], [11, 102, 255], [255, 7, 71], [255, 9, 224], [9, 7, 230],
+                [220, 220, 220], [255, 9, 92], [112, 9, 255], [8, 255, 214], [7, 255, 224],
+                [255, 184, 6], [10, 255, 71], [255, 41, 10], [7, 255, 255], [224, 255, 8],
+                [102, 8, 255], [255, 61, 6], [255, 194, 7], [255, 122, 8], [0, 255, 20], [255, 8, 41],
+                [255, 5, 153], [6, 51, 255], [235, 12, 255], [160, 150, 20], [0, 163, 255],
+                [140, 140, 140], [250, 10, 15], [20, 255, 0], [31, 255, 0], [255, 31, 0],
+                [255, 224, 0], [153, 255, 0], [0, 0, 255], [255, 71, 0], [0, 235, 255], [0, 173, 255],
+                [31, 0, 255], [11, 200, 200], [255, 82, 0], [0, 255, 245], [0, 61, 255], [0, 255, 112],
+                [0, 255, 133], [255, 0, 0], [255, 163, 0], [255, 102, 0], [194, 255, 0], [0, 143, 255],
+                [51, 255, 0], [0, 82, 255], [0, 255, 41], [0, 255, 173], [10, 0, 255], [173, 255, 0],
+                [0, 255, 153], [255, 92, 0], [255, 0, 255], [255, 0, 245], [255, 0, 102],
+                [255, 173, 0], [255, 0, 20], [255, 184, 184], [0, 31, 255], [0, 255, 61], [0, 71, 255],
+                [255, 0, 204], [0, 255, 194], [0, 255, 82], [0, 10, 255], [0, 112, 255], [51, 0, 255],
+                [0, 194, 255], [0, 122, 255], [0, 255, 163], [255, 153, 0], [0, 255, 10],
+                [255, 112, 0], [143, 255, 0], [82, 0, 255], [163, 255, 0], [255, 235, 0],
+                [8, 184, 170], [133, 0, 255], [0, 255, 92], [184, 0, 255], [255, 0, 31], [0, 184, 255],
+                [0, 214, 255], [255, 0, 112], [92, 255, 0], [0, 224, 255], [112, 224, 255],
+                [70, 184, 160], [163, 0, 255], [153, 0, 255], [71, 255, 0], [255, 0, 163],
+                [255, 204, 0], [255, 0, 143], [0, 255, 235], [133, 255, 0], [255, 0, 235],
+                [245, 0, 255], [255, 0, 122], [255, 245, 0], [10, 190, 212], [214, 255, 0],
+                [0, 204, 255], [20, 0, 255], [255, 255, 0], [0, 153, 255], [0, 41, 255], [0, 255, 204],
+                [41, 0, 255], [41, 255, 0], [173, 0, 255], [0, 245, 255], [71, 0, 255], [122, 0, 255],
+                [0, 255, 184], [0, 92, 255], [184, 255, 0], [0, 133, 255], [255, 214, 0],
+                [25, 194, 194], [102, 255, 0], [92, 0, 255]]
+
+
+stuff_classes = ['wall', 'building', 'sky', 'floor', 'tree', 'ceiling', 'road, route', 'bed',
+                 'window ', 'grass', 'cabinet', 'sidewalk, pavement', 'person', 'earth, ground',
+                 'door', 'table', 'mountain, mount', 'plant', 'curtain', 'chair', 'car', 'water',
+                 'painting, picture', 'sofa', 'shelf', 'house', 'sea', 'mirror', 'rug', 'field',
+                 'armchair', 'seat', 'fence', 'desk', 'rock, stone', 'wardrobe, closet, press', 'lamp',
+                 'tub', 'rail', 'cushion', 'base, pedestal, stand', 'box', 'column, pillar',
+                 'signboard, sign', 'chest of drawers, chest, bureau, dresser', 'counter', 'sand',
+                 'sink', 'skyscraper', 'fireplace', 'refrigerator, icebox',
+                 'grandstand, covered stand', 'path', 'stairs', 'runway',
+                 'case, display case, showcase, vitrine', 'pool table, billiard table, snooker table',
+                 'pillow', 'screen door, screen', 'stairway, staircase', 'river', 'bridge, span',
+                 'bookcase', 'blind, screen', 'coffee table',
+                 'toilet, can, commode, crapper, pot, potty, stool, throne', 'flower', 'book', 'hill',
+                 'bench', 'countertop', 'stove', 'palm, palm tree', 'kitchen island', 'computer',
+                 'swivel chair', 'boat', 'bar', 'arcade machine', 'hovel, hut, hutch, shack, shanty',
+                 'bus', 'towel', 'light', 'truck', 'tower', 'chandelier', 'awning, sunshade, sunblind',
+                 'street lamp', 'booth', 'tv', 'plane', 'dirt track', 'clothes', 'pole',
+                 'land, ground, soil', 'bannister, banister, balustrade, balusters, handrail',
+                 'escalator, moving staircase, moving stairway',
+                 'ottoman, pouf, pouffe, puff, hassock', 'bottle', 'buffet, counter, sideboard',
+                 'poster, posting, placard, notice, bill, card', 'stage', 'van', 'ship', 'fountain',
+                 'conveyer belt, conveyor belt, conveyer, conveyor, transporter', 'canopy',
+                 'washer, automatic washer, washing machine', 'plaything, toy', 'pool', 'stool',
+                 'barrel, cask', 'basket, handbasket', 'falls', 'tent', 'bag', 'minibike, motorbike',
+                 'cradle', 'oven', 'ball', 'food, solid food', 'step, stair', 'tank, storage tank',
+                 'trade name', 'microwave', 'pot', 'animal', 'bicycle', 'lake', 'dishwasher', 'screen',
+                 'blanket, cover', 'sculpture', 'hood, exhaust hood', 'sconce', 'vase',
+                 'traffic light', 'tray', 'trash can', 'fan', 'pier', 'crt screen', 'plate', 'monitor',
+                 'bulletin board', 'shower', 'radiator', 'glass, drinking glass', 'clock', 'flag']
+
+stuff_dataset_id_to_contiguous_id = {0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9,
+                                     10: 10, 11: 11, 12: 12, 13: 13, 14: 14, 15: 15, 16: 16, 17: 17,
+                                     18: 18, 19: 19, 20: 20, 21: 21, 22: 22, 23: 23, 24: 24, 25: 25,
+                                     26: 26, 27: 27, 28: 28, 29: 29, 30: 30, 31: 31, 32: 32, 33: 33,
+                                     34: 34, 35: 35, 36: 36, 37: 37, 38: 38, 39: 39, 40: 40, 41: 41,
+                                     42: 42, 43: 43, 44: 44, 45: 45, 46: 46, 47: 47, 48: 48, 49: 49,
+                                     50: 50, 51: 51, 52: 52, 53: 53, 54: 54, 55: 55, 56: 56, 57: 57,
+                                     58: 58, 59: 59, 60: 60, 61: 61, 62: 62, 63: 63, 64: 64, 65: 65,
+                                     66: 66, 67: 67, 68: 68, 69: 69, 70: 70, 71: 71, 72: 72, 73: 73,
+                                     74: 74, 75: 75, 76: 76, 77: 77, 78: 78, 79: 79, 80: 80, 81: 81,
+                                     82: 82, 83: 83, 84: 84, 85: 85, 86: 86, 87: 87, 88: 88, 89: 89,
+                                     90: 90, 91: 91, 92: 92, 93: 93, 94: 94, 95: 95, 96: 96, 97: 97,
+                                     98: 98, 99: 99, 100: 100, 101: 101, 102: 102, 103: 103, 104: 104,
+                                     105: 105, 106: 106, 107: 107, 108: 108, 109: 109, 110: 110,
+                                     111: 111, 112: 112, 113: 113, 114: 114, 115: 115, 116: 116,
+                                     117: 117, 118: 118, 119: 119, 120: 120, 121: 121, 122: 122,
+                                     123: 123, 124: 124, 125: 125, 126: 126, 127: 127, 128: 128,
+                                     129: 129, 130: 130, 131: 131, 132: 132, 133: 133, 134: 134,
+                                     135: 135, 136: 136, 137: 137, 138: 138, 139: 139, 140: 140,
+                                     141: 141, 142: 142, 143: 143, 144: 144, 145: 145, 146: 146,
+                                     147: 147, 148: 148, 149: 149}
+
+thing_classes = ['bed', 'window ', 'cabinet', 'person', 'door', 'table', 'curtain', 'chair', 'car',
+                 'painting, picture', 'sofa', 'shelf', 'mirror', 'armchair', 'seat', 'fence', 'desk',
+                 'wardrobe, closet, press', 'lamp', 'tub', 'rail', 'cushion', 'box', 'column, pillar',
+                 'signboard, sign', 'chest of drawers, chest, bureau, dresser', 'counter', 'sink',
+                 'fireplace', 'refrigerator, icebox', 'stairs',
+                 'case, display case, showcase, vitrine', 'pool table, billiard table, snooker table',
+                 'pillow', 'screen door, screen', 'bookcase', 'coffee table',
+                 'toilet, can, commode, crapper, pot, potty, stool, throne', 'flower', 'book', 'bench',
+                 'countertop', 'stove', 'palm, palm tree', 'kitchen island', 'computer',
+                 'swivel chair', 'boat', 'arcade machine', 'bus', 'towel', 'light', 'truck',
+                 'chandelier', 'awning, sunshade, sunblind', 'street lamp', 'booth', 'tv', 'plane',
+                 'clothes', 'pole', 'bannister, banister, balustrade, balusters, handrail',
+                 'ottoman, pouf, pouffe, puff, hassock', 'bottle', 'van', 'ship', 'fountain',
+                 'washer, automatic washer, washing machine', 'plaything, toy', 'stool',
+                 'barrel, cask', 'basket, handbasket', 'bag', 'minibike, motorbike', 'oven', 'ball',
+                 'food, solid food', 'step, stair', 'trade name', 'microwave', 'pot', 'animal',
+                 'bicycle', 'dishwasher', 'screen', 'sculpture', 'hood, exhaust hood', 'sconce',
+                 'vase', 'traffic light', 'tray', 'trash can', 'fan', 'plate', 'monitor',
+                 'bulletin board', 'radiator', 'glass, drinking glass', 'clock', 'flag']
+
+thing_dataset_id_to_contiguous_id = {7: 7, 8: 8, 10: 10, 12: 12, 14: 14, 15: 15, 18: 18, 19: 19,
+                                     20: 20, 22: 22, 23: 23, 24: 24, 27: 27, 30: 30, 31: 31, 32: 32,
+                                     33: 33, 35: 35, 36: 36, 37: 37, 38: 38, 39: 39, 41: 41, 42: 42,
+                                     43: 43, 44: 44, 45: 45, 47: 47, 49: 49, 50: 50, 53: 53, 55: 55,
+                                     56: 56, 57: 57, 58: 58, 62: 62, 64: 64, 65: 65, 66: 66, 67: 67,
+                                     69: 69, 70: 70, 71: 71, 72: 72, 73: 73, 74: 74, 75: 75, 76: 76,
+                                     78: 78, 80: 80, 81: 81, 82: 82, 83: 83, 85: 85, 86: 86, 87: 87,
+                                     88: 88, 89: 89, 90: 90, 92: 92, 93: 93, 95: 95, 97: 97, 98: 98,
+                                     102: 102, 103: 103, 104: 104, 107: 107, 108: 108, 110: 110,
+                                     111: 111, 112: 112, 115: 115, 116: 116, 118: 118, 119: 119,
+                                     120: 120, 121: 121, 123: 123, 124: 124, 125: 125, 126: 126,
+                                     127: 127, 129: 129, 130: 130, 132: 132, 133: 133, 134: 134,
+                                     135: 135, 136: 136, 137: 137, 138: 138, 139: 139, 142: 142,
+                                     143: 143, 144: 144, 146: 146, 147: 147, 148: 148, 149: 149}
+
+arkit_class_names = [
+    "cabinet", "refrigerator", "shelf", "stove", "bed", # 0..5
+    "sink", "washer", "toilet", "bathtub", "oven", # 5..10
+    "dishwasher", "fireplace", "stool", "chair", "table", # 10..15
+    "tv_monitor", "sofa", # 15..17
+]
+
+arkit_class_names_map = {
+    "cabinet": set(["cabinet", "chest of drawers, chest, bureau, dresser"]),
+    "refrigerator": set(["refrigerator, icebox"]),
+    "shelf": set(["shelf"]),
+    "stove": set(["stove"]),
+    "bed": set(["bed"]),
+    "sink": set(["sink"]),
+    "washer": set(["washer, automatic washer, washing machine"]),
+    "toilet": set(["toilet, can, commode, crapper, pot, potty, stool, throne"]),
+    "bathtub": set(["tub"]),
+    "oven": set(["oven"]),
+    "dishwasher": set(["dishwasher"]),
+    "fireplace": set(["fireplace"]),
+    # see toilet
+    "stool": set(["stool"]),
+    "chair": set(["chair", "armchair", "swivel chair", "seat"]),
+    "table": set(["pool table, billiard table, snooker table", "coffee table", "table", "desk"]),
+    "tv_monitor": set(["monitor", "crt screen"]),
+    "sofa": set(["sofa"]),
+}
+
+
+def test_arkit_class_names_map():
+
+    assert len(arkit_class_names_map) == len(arkit_class_names)
+    already_present = set()
+    for k,vs in arkit_class_names_map.items():
+        assert k in arkit_class_names
+        for v in vs:
+            assert not already_present.__contains__(v)
+            already_present.add(v)
+            assert v in stuff_classes
+
+
+if __name__ == "__main__":
+    test_arkit_class_names_map()

22be4fa025c3166a86318ffbcc24625b946220c5
Vaclav Vavra <mr.vaclav.vavra@gmail.com>
2024-02-20 15:58:09 +0100

    twelve scenes

--- local_infer.py
+++ local_infer.py
@@ -1,4 +1,5 @@
 import os
+import os.path as osp
 from pathlib import Path
 
 
@@ -18,6 +19,29 @@ def infer_me(in_dir, out_dir):
             infer_and_save(in_dir, file_name, out_dir)
 
 
-if __name__ == "__main__":
-    infer_me(in_dir="../twelve_scenes/kitchen/data/", out_dir="./twelve_scenes_infer/1")
+def only_dirs(d):
+    return [_ for _ in os.listdir(d) if osp.isdir(osp.join(d, _))]
 
+
+def compute(root_in_dir, root_out_dir="./twelve_scenes_infer_layout"):
+
+    # LAYOUT:
+    # root/scene(apt1)/room(kitchen)/info.txt
+
+    scenes = only_dirs(root_in_dir)
+    # scenes = ["apt1"]
+    scene_map = {}
+    for scene in scenes:
+        scene_map[scene] = []
+        rooms = only_dirs(osp.join(root_in_dir, scene))
+        # rooms = ["kitchen"]
+        for room in rooms:
+            in_path = os.path.join(root_in_dir, scene, room)
+            out_dir = os.path.join(root_out_dir, scene, room)
+            print(f"{in_path} => {out_dir}")
+            infer_me(in_dir=in_path, out_dir=out_dir)
+
+
+if __name__ == "__main__":
+    # compute("../twelve_scenes/data/ds")
+    compute("../twelve_scenes")
